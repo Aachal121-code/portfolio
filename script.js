693b79887d2c2ddb37482ca0a94cd00d81429499
Aachal121-code <aachalbhonde19@gmail.com>
2025-05-24 15:37:02 +0530

    fix images in block

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const btn1 = document.querySelector('#skills-btn');
 const btn2 = document.querySelector('#certificate-btn');
 
 const skillsData = document.querySelector(".skills-block");
-const certificateData = document.querySelector(".certificates");
 
 btn1.addEventListener('click', () => {
     datadisplay.innerHTML = skillsData.innerHTML;
-    datadisplay.classList.remove('certificateData');
 });
 
 btn2.addEventListener('click', () => {
-    datadisplay.innerHTML = certificateData.innerHTML;
-    datadisplay.classList.remove('skills-block');
+    datadisplay.innerHTML = `
+        <div class="certificates-block">
+            <img src="/certificates/c.png" />
+            <img src="/certificates/cpp.png" />
+            <img src="/certificates/python.png" />
+            <img src="/certificates/codeWbarriers.png" />
+        </div>
+    `;
 });
