e22ffbae0e5e895044d791c47a1d6554795381eb
Aachal121-code <aachalbhonde19@gmail.com>
2025-05-31 16:16:39 +0530

    fix displayarea

--- portfolio_script.js
+++ portfolio_script.js
@@ -2,9 +2,10 @@ const  profile = document.querySelector("#profile");
 const displayarea=document.querySelector(".displayarea");
 
 profile.addEventListener('click',()=>{
-    const profileInfo=document.createElement("div");
-    profileInfo.className="profile-info";
-    displayarea.appendChild(profileInfo);
-    
-    console.log("profile");
+    if(!document.querySelector(".profileInfo")){
+        const profileInfo=document.createElement("div");
+        profileInfo.className="profileInfo";
+        displayarea.appendChild(profileInfo);
+        console.log("profile");
+    } 
 })
