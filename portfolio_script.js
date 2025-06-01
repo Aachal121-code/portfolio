c347b4c2a7553497555de824f76b0a1aedd3c6c5
Aachal121-code <aachalbhonde19@gmail.com>
2025-06-01 13:23:46 +0530

    add profile logo

--- portfolio_script.js
+++ portfolio_script.js
@@ -1,11 +1,19 @@
 const  profile = document.querySelector("#profile");
 const displayarea=document.querySelector(".displayarea");
+displayarea.className="displayarea";
 
 profile.addEventListener('click',()=>{
     if(!document.querySelector(".profileInfo")){
         const profileInfo=document.createElement("div");
         profileInfo.className="profileInfo";
         displayarea.appendChild(profileInfo);
-        console.log("profile");
+
+        const profileLogo = document.createElement("i");
+        profileLogo.className = "profileLogo";
+        profileLogo.innerHTML = `<i class="fa fa-user-circle" aria-hidden="true"></i>`;
+        profileInfo.appendChild(profileLogo);
+
+        
+
     } 
 })
